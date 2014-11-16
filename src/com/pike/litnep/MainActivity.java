0138c7ea28740841c6fec504ac42e2edea81a3dc
Bishal Heuju <b.heuju@yahoo.com>
2014-11-16 23:34:03 +0545

    actionBar tabs, fragments, and swiping (viewPage) added

--- src/com/pike/litnep/MainActivity.java
+++ src/com/pike/litnep/MainActivity.java
@@ -1,31 +1,83 @@
 package com.pike.litnep;
 
+import com.pike.litnep.adapter.TabsPagerAdapter;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBar.Tab;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements
+		ActionBar.TabListener {
 
 	private Button hideActionBar;
-	private TextView tvStatus;
+	// private TextView tvStatus;
 	private boolean statusActionBar = false;
+
+	private ViewPager mViewPager;
+	private TabsPagerAdapter mAdapter;
+	private ActionBar actionBar;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		// Initializations
+		mViewPager = (ViewPager) findViewById(R.id.pager);
+		actionBar = getSupportActionBar();
+		mAdapter = new TabsPagerAdapter(getSupportFragmentManager());
+
+		mViewPager.setAdapter(mAdapter);
+		actionBar.setHomeButtonEnabled(false);
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+		// Set 3 tabs icons and/or titles and setting tabListeners
+		Tab tab1 = actionBar.newTab().setText("Tab1").setTabListener(this);
+		Tab tab2 = actionBar.newTab().setText("Tab2").setTabListener(this);
+		Tab tab3 = actionBar.newTab().setText("Tab3").setTabListener(this);
+
+		// Add tabs to actionBar
+		actionBar.addTab(tab1);
+		actionBar.addTab(tab2);
+		actionBar.addTab(tab3);
+
+		/** Select appropriate tab on swiping
+		 ********************************************* */
+		mViewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+			
+			@Override
+			public void onPageSelected(int position) {
+				// TODO Auto-generated method stub
+				actionBar.setSelectedNavigationItem(position);
+			}
+			
+			@Override
+			public void onPageScrolled(int arg0, float arg1, int arg2) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void onPageScrollStateChanged(int arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
 		
-		/** Listener for Hiding and Displaying ActionBar
+		/**
+		 * Listener for Hiding and Displaying ActionBar
 		 *************************************************/
 		{
 			hideActionBar = (Button) findViewById(R.id.btnHideActionBar);
-			tvStatus = (TextView) findViewById(R.id.tvStatus);
+			// tvStatus = (TextView) findViewById(R.id.tvStatus);
 			hideActionBar.setOnClickListener(new View.OnClickListener() {
 				@Override
 				public void onClick(View v) {
@@ -35,16 +87,16 @@ public class MainActivity extends ActionBarActivity {
 					ActionBar actionBar = getSupportActionBar();
 					if (statusActionBar) {
 						actionBar.hide();
-						tvStatus.setText("Hidden");
+						// tvStatus.setText("Hidden");
 						hideActionBar.setText("Show");
 					} else {
 						actionBar.show();
-						tvStatus.setText("Displayed");
+						// tvStatus.setText("Displayed");
 						hideActionBar.setText("Hide");
 					}
 				}
 			});
-		}
+		}// end buttonListener
 
 	}
 
@@ -76,19 +128,43 @@ public class MainActivity extends ActionBarActivity {
 
 	}
 
-	
-	/** ActionBar functions 
+	/**
+	 * ActionBar functions
 	 *******************************/
 	private void openCompose() {
-		Intent intent = new Intent (this, ComposeActivity.class);
+		Intent intent = new Intent(this, ComposeActivity.class);
 		startActivity(intent);
 	}
+
 	private void openSignin() {
-		Intent intent = new Intent (this, SigninActivity.class);
+		Intent intent = new Intent(this, SigninActivity.class);
 		startActivity(intent);
 	}
+
 	private void openSettings() {
-		
+
+	}
+
+	/**
+	 * ActionBar Tabs functions
+	 *******************************/
+	@Override
+	public void onTabSelected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+		// show
+		mViewPager.setCurrentItem(tab.getPosition());
+	}
+
+	@Override
+	public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+		// remove
+	}
+
+	@Override
+	public void onTabReselected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+		// ignore
 	}
 
 }
---
+++ src/com/pike/litnep/adapter/TabsPagerAdapter.java
@@ -0,0 +1,38 @@
+package com.pike.litnep.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.pike.litnep.FragmentTab1;
+import com.pike.litnep.FragmentTab2;
+import com.pike.litnep.FragmentTab3;
+
+public class TabsPagerAdapter extends FragmentPagerAdapter {
+
+	public TabsPagerAdapter(FragmentManager fm) {
+		super(fm);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Fragment getItem(int index) {
+		switch (index)
+		{
+		case 0:
+			return new FragmentTab1();
+		case 1:
+			return new FragmentTab2();
+		case 2:
+			return new FragmentTab3();
+		}
+		return null;
+	}
+
+	@Override
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return 3;
+	}
+
+}
